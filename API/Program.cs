66ecfd36c171090faa2c88d1fabf705433b5bb8c
mattepusce <matte.psc@gmail.com>
2025-03-08 18:24:11 +0100

    - Creazione entity Activity

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,5 @@
 
-// per la Clear Architecture creare 4 progetti
+// per la Clean Architecture creare 4 progetti
 // API, Application, Domain, Persistence
 // aggiungere i progetti alla soluzione (dotnet sln add NomeProgetto)
 // aggiungere al progetto API il riferimento al progetto Application
@@ -11,20 +11,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
