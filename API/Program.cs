d29f7ea955cdccab8ec14631c9761513bbd5e5a1
mattepusce <matte.psc@gmail.com>
2025-03-08 17:39:39 +0100

    - Aggiunta una descrizione della Clear Architecture in Program.cs - Aggiunti i Project References

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,11 @@
+
+// per la Clear Architecture creare 4 progetti
+// API, Application, Domain, Persistence
+// aggiungere i progetti alla soluzione (dotnet sln add NomeProgetto)
+// aggiungere al progetto API il riferimento al progetto Application
+// aggiungere al progetto Application il riferimento al progetto Domain e Persistence
+// aggiungere al progetto Persistence il riferimento al progetto Domain
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
