7c5255b0eb774a4dfb0c9700f63395bd01c18767
mattepusce <matte.psc@gmail.com>
2025-03-15 11:52:19 +0100

    [Client] - aggiunta Material UI

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,3 +1,4 @@
+import { List, ListItem, ListItemText, Typography } from '@mui/material';
 import { useEffect, useState } from 'react'
 
 function App() {
@@ -14,14 +15,16 @@ function App() {
 
   return (
     <>
-      <h3 className="app" >Reactivities</h3>
-      <ul>
+      <Typography variant='h3'>Reactivities</Typography>
+      <List>
         {
           activities.map(activity => (
-            <li key={activity.id}>{activity.title}</li>
+            <ListItem key={activity.id}>
+              <ListItemText>{activity.title}</ListItemText>
+            </ListItem>
           ))
         }
-      </ul>
+      </List>
     </>
   )
 }
