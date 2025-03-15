4345653b7184050b6575914111808b76fe104da3
mattepusce <matte.psc@gmail.com>
2025-03-15 12:04:50 +0100

    [Client] - utilizzo di Axios per le chiamate API

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,32 +1,31 @@
-import { List, ListItem, ListItemText, Typography } from '@mui/material';
-import { useEffect, useState } from 'react'
+import { List, ListItem, ListItemText, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+// npm install axios
+import axios from "axios";
 
 function App() {
- 
   const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    fetch("https://localhost:5001/api/activities")
-      .then(response => response.json())
-      .then(data => setActivities(data))
+    axios
+      .get<Activity[]>("https://localhost:5001/api/activities")
+      .then((response) => setActivities(response.data));
 
-      return () => {}
+    return () => {};
   }, []);
 
   return (
     <>
-      <Typography variant='h3'>Reactivities</Typography>
+      <Typography variant="h3">Reactivities</Typography>
       <List>
-        {
-          activities.map(activity => (
-            <ListItem key={activity.id}>
-              <ListItemText>{activity.title}</ListItemText>
-            </ListItem>
-          ))
-        }
+        {activities.map((activity) => (
+          <ListItem key={activity.id}>
+            <ListItemText>{activity.title}</ListItemText>
+          </ListItem>
+        ))}
       </List>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+//npm install -D vite-plugin-mkcert serve per installare il plugin per il certificato e usare https in locale
+// funziona solo durante lo sviluppo e non in produzione
+import mkcert from 'vite-plugin-mkcert'
+
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [react()],
+  plugins: [
+    react(), 
+    mkcert()
+  ],
 })
